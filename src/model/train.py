ae104cd602c9d76340bf6c1d5a72919069563ae3
Andrei.Aksionov <aksionau.andrei@gmail.com>
2023-02-10 19:32:25 +0300

    Explicitly provide device for training

--- src/model/train.py
+++ src/model/train.py
@@ -11,7 +11,7 @@ from src.model import BigramLanguageModel, GPTLanguageModel, Trainer
 from src.utils import get_device, grab_arguments, set_seed
 
 
-def train(model_class: torch.nn.Module, *, debug: bool) -> None:
+def train(model_class: torch.nn.Module, device: str | None, *, debug: bool) -> None:
     """Train a language model.
 
     Performs 4 steps:
@@ -24,6 +24,9 @@ def train(model_class: torch.nn.Module, *, debug: bool) -> None:
     ----------
     model_class : torch.nn.Module
         what language model to use
+    device: str | None
+        on what device to train, if not provided will try to figure out what device to use such as:
+        if gpu (cuda or mps) is available will use it, if not - cpu
     debug : bool
         GPT model has two configs: small and large. Small is used for debug purpose as it's fast
 
@@ -81,7 +84,8 @@ def train(model_class: torch.nn.Module, *, debug: bool) -> None:
     logger.info("Staring training...")
     model = model_class(vocab_size=tokenizer.vocab_size, **grab_arguments(model_class, model_config))
     optimizer = torch.optim.AdamW(model.parameters(), lr=model_config.learning_rate)
-    trainer = Trainer(model, optimizer, train_dataloader, test_dataloader, device=get_device())
+    device = device if device else get_device()
+    trainer = Trainer(model, optimizer, train_dataloader, test_dataloader, device=device)
     trainer.train(epochs=model_config.epochs)
     logger.info("Training is finished")
 
@@ -89,19 +93,29 @@ def train(model_class: torch.nn.Module, *, debug: bool) -> None:
 def main() -> None:
     """Train either GPT or a simple bigram language model on tiny-shakespeare dataset."""
     parser = argparse.ArgumentParser(description="Train bigram or GPT language model")
-    models = {
-        "bigram": BigramLanguageModel,
-        "gpt": GPTLanguageModel,
-    }
-    parser.add_argument("--model", "-m", type=str, help="Bigram or GPT", choices=list(models), required=True)
+    models = {"bigram": BigramLanguageModel, "gpt": GPTLanguageModel}
+    parser.add_argument(
+        "--model",
+        "-m",
+        choices=list(models),
+        help="Bigram or GPT",
+        required=True,
+        type=str,
+    )
     parser.add_argument(
         "--debug",
         "-d",
         action="store_true",
         help="Do you want to run fast training for debug purpose?",
     )
+    parser.add_argument(
+        "--device",
+        help="Optionally you can select device on which the model will be trained",
+        required=False,
+        type=str,
+    )
     args = parser.parse_args()
-    train(models[args.model], debug=args.debug)
+    train(models[args.model], args.device, debug=args.debug)
 
 
 if __name__ == "__main__":
