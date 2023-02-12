dc95b7130b9cf992f916ed031c18cb25d218ca86
Andrei.Aksionov <aksionau.andrei@gmail.com>
2023-02-12 21:38:29 +0300

    Causal Self Attention: cleaner code

--- src/model/gpt_language_model/attention.py
+++ src/model/gpt_language_model/attention.py
@@ -224,65 +224,42 @@ class CausalSelfAttention(nn.Module):
         self.embeddings_size = embeddings_size
         self.context_size = context_size
         self.num_heads = num_heads
-        # self.bias = bias
+        self.bias = bias
         self.dropout = dropout
 
-        # key, query and value projections for all heads in a single batch, hence `* 3`
-        self.causal_self_attention = nn.Linear(embeddings_size, embeddings_size * 3, bias=bias)
+        # key, query and value projections (hence `3 * embeddings_size`) for all heads in a single batch
+        self.causal_self_attention = nn.Linear(embeddings_size, 3 * embeddings_size, bias=bias)
         # output projection
-        self.causal_projection = nn.Linear(embeddings_size, embeddings_size, bias=bias)
+        self.projection = nn.Linear(embeddings_size, embeddings_size, bias=bias)
         # regularization
         self.attention_dropout = nn.Dropout(self.dropout)
-        self.residual_dropout = nn.Dropout(self.dropout)
-
-        # TODO: why it's called 'bias'?
-        self.register_buffer(
-            "bias",
-            torch.tril(
-                torch.ones(self.context_size, self.context_size),
-            ).view(1, 1, self.context_size, self.context_size),
-        )
-        # TODO: this should also work
-        # self.register_buffer("bias", torch.tril(torch.ones(1, 1, self.context_size, self.context_size)))
-        # TODO: most likely we don't need 4 dim tril, 2 dim is enough
+        self.projection_dropout = nn.Dropout(self.dropout)
+        # triangular matrix for masking 'future' tokens
+        self.register_buffer("tril", torch.tril(torch.ones(self.context_size, self.context_size)))
 
     def forward(self, x: Tensor) -> Tensor:
         # batch, sequence length, embedding size
-        B, T, C = x.shape
+        B, T, C = x.shape  # noqa: N806
 
-        # calculate query, key and value for all heads in the batch and split into 3 chunks
-        # TODO: most likely (B, T, C // 3)
-        # temp = self.causal_self_attention(x) # (B, T, C * 3)
-        # query, key, value = self.causal_self_attention(x).split(self.embeddings_size, dim=2)  # TODO: maybe dim=-1
-        # query, key, value = temp.split(self.embeddings_size, dim=2) # (B, T, C)
+        query, key, value = self.causal_self_attention(x).split(
+            self.embeddings_size,
+            dim=-1,
+        )  # (B, T, C) -> (B, T, C * 3) -> (B, T, C)
 
-        # (B, T, C) -> (B, T, C * 3) -> (B, T, C)
-        query, key, value = self.causal_self_attention(x).split(self.embeddings_size, dim=2)  # TODO: maybe dim=-1
-
-        # TODO: is transpose(1,2) == .mT?
         key = key.view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)  # (B, nh, T, hs)
-        # key = key.view(B, T, self.num_heads, -1).transpose(1, 2)  # (B, nh, T, hs)
         query = query.view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)  # (B, nh, T, hs)
         value = value.view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)  # (B, nh, T, hs)
 
-        attention_scores: Tensor = query @ key.mT  # (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        head_size = key.shape[-1]
-        attention_scores *= head_size**-0.5  # (B, nh, T, T)
-
-        # NOTE: if to use 2 dim tril -> self.bias[:T, :T]
-        attention_scores = attention_scores.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))  # (B, nh, T, T)
-
+        attention_scores = query @ key.mT  # (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        attention_scores *= key.shape[-1] ** -0.5  # (B, nh, T, T)
+        attention_scores = attention_scores.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # (B, nh, T, T)
         attention_scores = F.softmax(attention_scores, dim=-1)  # (B, nh, T, T)
         attention_scores = self.attention_dropout(attention_scores)  # (B, nh, T, T)
 
-        y = attention_scores @ value  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        output = attention_scores @ value  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        output = output.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
+        # output projection
+        output = self.projection(output)
+        output = self.projection_dropout(output)
 
-        y = y.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
-
-        # out projection
-        y = self.causal_projection(y)
-        # TODO: why do we need residual dropout here? Maybe wrong naming?
-        # perhaps proj_dropout
-        y = self.residual_dropout(y)
-
-        return y
+        return output
