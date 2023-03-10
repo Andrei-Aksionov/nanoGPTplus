97b0a3f63d3a7ea1b43956461e96903afbc61664
Andrei-Aksionov <58434077+Andrei-Aksionov@users.noreply.github.com>
2023-03-10 16:08:40 +0300

    Num workers are not larger than number of CPU cores (#12)
    * Training and Generation improvements
    
    1. Fix seed for token generation
    2. Initial context for token generation
    3. Better config for LR scheduler
    4. Write logs to a file
    5. Number of worker for DataLoader does not exceed number of available CPU cores

--- tests/smoke/generate_test.py
+++ tests/smoke/generate_test.py
@@ -11,13 +11,15 @@ class TestTokenGeneration:
     @pytest.mark.parametrize("model_size", list(config.model.bigram.size.keys()))
     def test_bigram_token_generation(self, model_size: str) -> None:
         completed_process = subprocess.run(
-            f"python src/model/generate.py --model bigram --size {model_size} --device cpu --max-new-tokens 10".split(),
+            f"python src/model/generate.py --model bigram --size {model_size} "
+            "--device cpu --max-new-tokens 10 --fix-seed".split(),
         )
         assert completed_process.returncode == 0
 
     @pytest.mark.parametrize("model_size", list(config.model.gpt.size.keys()))
     def test_gpt_token_generation(self, model_size: str) -> None:
         completed_process = subprocess.run(
-            f"python src/model/generate.py --model gpt --size {model_size} --device cpu --max-new-tokens 10".split(),
+            f"python src/model/generate.py --model gpt --size {model_size} "
+            "--device cpu --max-new-tokens 10 --fix-seed".split(),
         )
         assert completed_process.returncode == 0
