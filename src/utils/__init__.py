8dcec0801f896224385a9274e7b202c5373d59ba
Andrei.Aksionov <aksionau.andrei@gmail.com>
2023-03-15 19:21:00 +0300

    Black formatter: target version in pyproject.toml

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,6 @@
 from src.utils.arguments import RangeChecker, grab_arguments
 from src.utils.device import get_device
+from src.utils.error import log_error
 from src.utils.model import (
     get_model_config,
     load_checkpoint,
